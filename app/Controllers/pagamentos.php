0e9505a70ddc6ba1395d92ceef9defaa372fbd4c
Cleber F. Andrade <cleberf.andrade@hotmail.com>
2023-11-17 16:58:04 -0300

    NOVO

--- app/Controllers/pagamentos.php
+++ app/Controllers/pagamentos.php
@@ -6,8 +6,31 @@ use Libraries\Check;
 use Libraries\Sessao;
 use Libraries\Url;
 
+use App\Models\Empresas;
+use App\Models\Financas;
+use App\Models\Usuarios;
+use App\Models\UsuariosEmpresa;
+
 class pagamentos extends View
 {
     private $dados = [];
     private $link,$Financas,$Check,$Usuarios,$UsuariosEmpresa;
+    public function __construct()
+    {
+        Sessao::naoLogado();
+        $this->dados['title'] = 'MÓDULO | FINANCEIRO >>'; 
+        $this->Financas = new Financas;  
+        $this->Check = new Check;
+        $this->Usuarios = new Usuarios;
+        $this->UsuariosEmpresa = new UsuariosEmpresa;
+        $this->dados['empresa'] = $this->UsuariosEmpresa->setCodEmpresa($_SESSION['EMP_COD'])->setCodUsuario($_SESSION['USU_COD'])->listar(0);
+        $this->dados['usuario'] = $this->Usuarios->setCodUsuario($_SESSION['USU_COD'])->listar(0);
+    }
+
+    public function index()
+    {
+        $this->dados['title'] .= ' GERENCIAR CONTAS';   
+        $this->dados['breadcrumb'] = $this->Check->setLink($this->link)->breadcrumb();
+        $this->render('admin/financeiro/pagamentos/listar', $this->dados);
+    }
 }
