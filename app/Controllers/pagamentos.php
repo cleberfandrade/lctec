7e15903273e3e4a3b969cc8902740f11665e86f7
Cleber F. Andrade <cleberf.andrade@hotmail.com>
2023-11-23 13:18:49 -0300

    ajustes e correções

--- app/Controllers/pagamentos.php
+++ app/Controllers/pagamentos.php
@@ -10,12 +10,12 @@ use App\Models\Empresas;
 use App\Models\Financas;
 use App\Models\Usuarios;
 use App\Models\UsuariosEmpresa;
-use App\Models\ContasPagarReceber;
+use App\Models\Pagamentos as ModelsPagamentos;
 
 class pagamentos extends View
 {
     private $dados = [];
-    private $link,$Financas,$Check,$Usuarios,$UsuariosEmpresa, $ContasPagarReceber;
+    private $link,$Financas,$Check,$Usuarios,$UsuariosEmpresa, $Pagamentos;
     public function __construct()
     {
         Sessao::naoLogado();
@@ -24,10 +24,10 @@ class pagamentos extends View
         $this->Check = new Check;
         $this->Usuarios = new Usuarios;
         $this->UsuariosEmpresa = new UsuariosEmpresa;
-        $this->ContasPagarReceber = new ContasPagarReceber;
+        $this->Pagamentos = new ModelsPagamentos;
         $this->dados['empresa'] = $this->UsuariosEmpresa->setCodEmpresa($_SESSION['EMP_COD'])->setCodUsuario($_SESSION['USU_COD'])->listar(0);
         $this->dados['usuario'] = $this->Usuarios->setCodUsuario($_SESSION['USU_COD'])->listar(0);
-        $this->dados['contas_pagar_receber'] = $this->ContasPagarReceber->setCodEmpresa($_SESSION['EMP_COD'])->listarTodos(0);
+        $this->dados['pagamentos'] = $this->Pagamentos->setCodEmpresa($_SESSION['EMP_COD'])->listarTodos(0);
     }
 
     public function index()
@@ -35,5 +35,12 @@ class pagamentos extends View
         $this->dados['title'] .= ' GERENCIAR PAGAMENTOS';   
         $this->dados['breadcrumb'] = $this->Check->setLink($this->link)->breadcrumb();
         $this->render('admin/financeiro/pagamentos/listar', $this->dados);
+    }
+    public function cadastro():void
+    {
+        $this->dados['title'] .= ' CADASTRAR PAGAMENTOS';
+        $this->link[3] = ['link'=> 'pagamentos/cadastrar','nome' => 'CADASTRO DE PAGAMENTOS'];
+        $this->dados['breadcrumb'] = $this->Check->setLink($this->link)->breadcrumb();
+        $this->render('admin/financeiro/pagamentos/cadastrar', $this->dados);
     }
 }
