04480259b0d64cd391f7683afc991a291e7050a1
cleberfandrade <cleberf.andrade@hotmail.com>
2023-12-24 15:39:18 -0300

    On branch main

---
+++ app/Models/Tarefas.php
@@ -0,0 +1,107 @@
+<?php
+namespace App\Models;
+
+use Core\Model;
+
+class Tarefas extends Model
+{ 
+    private $tabela = 'tb_tarefas';
+    private $Model = '';
+    private $codigo, $descricao, $tipo, $codEmpresa, $data;
+
+    public function __construct()
+    {
+        $this->Model = new Model();
+        $this->Model->setTabela($this->tabela);
+    }
+    public function setCodigo($codigo)
+    {
+        $this->codigo = $codigo;
+        return $this;
+    }
+    public function setDescricao($descricao)
+    {
+        $this->descricao = $descricao;
+        return $this;
+    }
+    public function setTipo($tipo)
+    {
+        $this->tipo = $tipo;
+        return $this;
+    }
+    public function setCodEmpresa($codEmpresa)
+    {
+        $this->codEmpresa = $codEmpresa;
+        return $this;
+    }
+    public function listar($ver = 0)
+    {
+        $parametros = "T INNER JOIN tb_empresas E ON T.EMP_COD=E.EMP_COD WHERE T.EMP_COD={$this->codEmpresa} AND T.TRF_COD={$this->codigo}";
+        $campos = "*";
+        $resultado = $this->Model->exibir($parametros, $campos, $ver, $id = false);
+        if ($resultado) {
+            return $resultado[0];
+        } else {
+            return false;
+        }
+    }
+    public function listarTodos($ver = 0)
+    {
+        $parametros = "T INNER JOIN tb_empresas E ON E.EMP_COD=T.EMP_COD WHERE T.EMP_COD={$this->codEmpresa} ORDER BY T.TRF_DT_CADASTRO";
+        $campos = "*";
+        $resultado = $this->Model->exibir($parametros, $campos, $ver, $id = false);
+        if ($resultado) {
+            return $resultado;
+        } else {
+            return false;
+        }
+    }
+    public function cadastrar(array $dados, $ver = 0)
+    {
+        $ok = $this->Model->cadastrar($dados, $ver);
+        if ($ok) {
+            return $ok;
+        } else {
+            return false;
+        }
+    }
+    public function alterar(array $dados, $ver = 0)
+    {
+        $parametros = "WHERE EMP_COD={$this->codEmpresa} AND TRF_COD=";
+        $this->Model->setParametros($parametros);
+        $this->Model->setCodigo($this->codigo);
+        $ok = false;
+        $ok = $this->Model->alterar($dados, $ver);
+        if ($ok) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function excluir($ver = 0)
+    {
+        $parametros = "WHERE EMP_COD='{$this->codEmpresa}' AND TRF_COD=";
+        $this->Model->setParametros($parametros);
+        $this->Model->setCodigo($this->codigo);
+        $ok = false;
+        $ok = $this->Model->deletar($ver);
+        if ($ok) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function checarDescricao()
+    {
+        $parametros = "WHERE EMP_COD={$this->codEmpresa} AND TRF_DESCRICAO='{$this->descricao}' AND TRF_DATA='{$this->data}'";
+        $campos = "*";
+        $resultado = $this->Model->exibir($parametros, $campos, $ver = 0, $id = false);
+        if ($resultado) {
+            //Já existe
+            return $resultado[0];
+        } else {
+            //Nao existe
+            return false;
+        }
+    }
+}
