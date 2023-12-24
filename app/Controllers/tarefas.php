281e2bdebc7b9925e976a221634196082fa44628
cleberfandrade <cleberf.andrade@hotmail.com>
2023-12-24 12:29:45 -0300

    On branch main

--- app/Controllers/tarefas.php
+++ app/Controllers/tarefas.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Controllers;
 
+use App\Models\Usuarios;
+use App\Models\UsuariosEmpresa;
 
 use Core\View;
 use Libraries\Check;
@@ -15,19 +17,28 @@ class tarefas extends View
     public function __construct()
     {
         Sessao::naoLogado();
-        $this->dados['title'] = 'LC-TEC | MINHAS TAREFAS >>'; 
+        $this->dados['title'] = 'MÓDULO | CADASTROS >>'; 
         $this->Check = new Check;
+        $this->Usuarios = new Usuarios;
+        $this->UsuariosEmpresa = new UsuariosEmpresa;
 
         $this->dados['empresa'] = $this->UsuariosEmpresa->setCodEmpresa($_SESSION['EMP_COD'])->setCodUsuario($_SESSION['USU_COD'])->listar(0);
         $this->dados['usuario'] = $this->Usuarios->setCodUsuario($_SESSION['USU_COD'])->listar(0);
         
         $this->link[0] = ['link'=> 'admin','nome' => 'PAINEL ADMINISTRATIVO'];
-        $this->link[1] = ['link'=> 'financeiro','nome' => 'TAREFAS'];
+        $this->link[1] = ['link'=> 'cadastros','nome' => 'MÓDULO DE CADASTROS'];
+        $this->link[2] = ['link'=> 'tarefas','nome' => 'GERENCIAR TAREFAS'];
     }
     public function index()
     {
         $this->dados['title'] .= ' GERENCIAR TAREFAS';   
         $this->dados['breadcrumb'] = $this->Check->setLink($this->link)->breadcrumb();
-        $this->render('admin/tarefas', $this->dados);
+        $this->render('admin/cadastros/tarefas/listar', $this->dados);
+    }
+    public function colunas()
+    {
+        $this->dados['title'] .= ' GERENCIAR COLUNAS TAREFAS';   
+        $this->dados['breadcrumb'] = $this->Check->setLink($this->link)->breadcrumb();
+        $this->render('admin/cadastros/tarefas/colunas', $this->dados);
     }
 }
