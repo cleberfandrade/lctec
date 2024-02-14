30dc25939f3813ed0db9c8f344b29b46a5c9c8c3
cleberfandrade <cleberf.andrade@hotmail.com>
2024-02-14 17:54:29 -0300

    On branch main

--- app/Controllers/suporte.php
+++ app/Controllers/suporte.php
@@ -23,6 +23,10 @@ class suporte extends View
         $this->Empresa = new Empresas;
         $this->Usuarios = new Usuarios;
         $this->UsuariosEmpresa = new UsuariosEmpresa;
+        $this->Check = new Check;
+
+        $this->dados['empresa'] = $this->UsuariosEmpresa->setCodEmpresa($_SESSION['EMP_COD'])->setCodUsuario($_SESSION['USU_COD'])->listar(0);
+        $this->dados['usuario'] = $this->Usuarios->setCodUsuario($_SESSION['USU_COD'])->listar(0);
 
         $this->link[0] = ['link'=> 'admin','nome' => 'PAINEL ADMINISTRATIVO'];
         $this->link[1] = ['link'=> 'cadastros','nome' => 'SUPORTE AO SISTEMA'];
@@ -31,7 +35,6 @@ class suporte extends View
     {
         $this->dados['title'] .= ' SUPORTE AO SISTEMA';   
         $this->dados['breadcrumb'] = $this->Check->setLink($this->link)->breadcrumb();
-        
         $this->render('admin/suporte', $this->dados);
     }
 }
