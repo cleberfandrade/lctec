c74e54a10a091a9c969a15ff73ea0efd19765dd4
cleberfandrade <cleberf.andrade@hotmail.com>
2024-02-22 12:12:23 -0300

    On branch main

---
+++ app/Models/Avisos.php
@@ -0,0 +1,105 @@
+<?php
+namespace App\Models;
+
+use Core\Model;
+
+class Categorias extends Model
+{ 
+    private $tabela = 'tb_avisos';
+    private $Model = '';
+    private $codigo,$codEmpresa,$descricao,$tipo;
+
+    public function __construct()
+    {
+        $this->Model = new Model();
+        $this->Model->setTabela($this->tabela);
+    }
+    public function setCodigo($codigo)
+    {
+        $this->codigo = $codigo;
+        return $this;
+    }
+    public function setCodEmpresa($codEmpresa)
+    {
+        $this->codEmpresa = $codEmpresa;
+        return $this;   
+    }
+    public function setDescricao($descricao)
+    {
+        $this->descricao = $descricao;
+        return $this;
+    }
+    public function setTipo($tipo)
+    {
+        $this->tipo = $tipo;
+        return $this;
+    }
+    public function listar($ver = 0)
+    {
+        $parametros = "C INNER JOIN tb_empresas E ON C.EMP_COD=E.EMP_COD WHERE C.EMP_COD={$this->codEmpresa} AND C.CAT_COD={$this->codigo}";
+        $campos = "*";
+        $resultado = $this->Model->exibir($parametros, $campos, $ver, $id = false);
+        if ($resultado) {
+            return $resultado[0];
+        } else {
+            return false;
+        }
+    }
+    public function listarTodosPorTipo($ver = 0)
+    {
+        $parametros = "C INNER JOIN tb_empresas E ON C.EMP_COD=E.EMP_COD WHERE C.EMP_COD={$this->codEmpresa} AND C.CAT_TIPO={$this->tipo} ORDER BY C.CAT_DESCRICAO";
+        $campos = "*";
+        $resultado = $this->Model->exibir($parametros, $campos, $ver = 0, $id = false);
+        if ($resultado) {
+            return $resultado;
+        } else {
+            return false;
+        }
+    }
+    public function listarTodos($ver = 0)
+    {
+        $parametros = "WHERE EMP_COD={$this->codEmpresa} ORDER BY CAT_DESCRICAO";
+        $campos = "*";
+        $resultado = $this->Model->exibir($parametros, $campos, $ver = 0, $id = false);
+        if ($resultado) {
+            return $resultado;
+        } else {
+            return false;
+        }
+    }
+    public function cadastrar(array $dados, $ver = 0)
+    {
+        $ok = $this->Model->cadastrar($dados, $ver);
+        if ($ok) {
+            return $ok;
+        } else {
+            return false;
+        }
+    }
+    public function excluir(array $dados, $ver = 0)
+    {
+        $parametros = " WHERE EMP_COD={$this->codEmpresa} AND CAT_COD=";
+        $this->Model->setParametros($parametros);
+        $this->Model->setCodigo($this->codigo);
+        $ok = false;
+        $ok = $this->Model->deletar($dados, $ver);
+        if ($ok) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function checarDescricao()
+    {
+        $parametros = "WHERE EMP_COD={$this->codEmpresa} AND CAT_DESCRICAO ='{$this->descricao}' AND CAT_STATUS=1";
+        $campos = "*";
+        $resultado = $this->Model->exibir($parametros, $campos, $ver = 0, $id = false);
+        if ($resultado) {
+            //Já existe
+            return $resultado[0];
+        } else {
+            //Nao existe
+            return false;
+        }
+    }
+}
