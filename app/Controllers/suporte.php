c041c0fe8f0b2ae0c364c0f3f9ce24dddd0bbe8a
cleberfandrade <cleberf.andrade@hotmail.com>
2024-02-25 15:19:28 -0300

    main

--- app/Controllers/suporte.php
+++ app/Controllers/suporte.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controllers;
 
+use App\Models\Chat;
 use App\Models\Classificacoes;
 use App\Models\Empresas;
 use App\Models\Suporte as ModelsSuporte;
@@ -15,7 +16,7 @@ use Libraries\Url;
 class suporte extends View
 {
     private $dados = [];
-    private $link,$Check,$Empresa, $Usuarios,$UsuariosEmpresa,$Suporte;
+    private $link,$Check,$Empresa, $Usuarios,$UsuariosEmpresa,$Suporte,$Chat;
 
     public function __construct()
     {
@@ -29,6 +30,8 @@ class suporte extends View
         $this->Suporte = new ModelsSuporte;
 
         $this->dados['suporte_usuario'] = $this->Suporte->setCodEmpresa($_SESSION['EMP_COD'])->setCodUsuario($_SESSION['USU_COD'])->listarTodasMensagensUsuario(0);
+
+        $this->Chat = new Chat;
 
         $this->dados['empresa'] = $this->UsuariosEmpresa->setCodEmpresa($_SESSION['EMP_COD'])->setCodUsuario($_SESSION['USU_COD'])->listar(0);
         $this->dados['usuario'] = $this->Usuarios->setCodUsuario($_SESSION['USU_COD'])->listar(0);
@@ -50,25 +53,56 @@ class suporte extends View
         if (isset($_POST) && isset($dados['CADASTRAR_NOVA_MENSAGEM'])) {
             unset($dados['CADASTRAR_NOVA_MENSAGEM']);
             if( $this->dados['empresa']['USU_COD'] == $dados['USU_COD'] && $this->dados['empresa']['EMP_COD'] == $dados['EMP_COD']){
-                
+                $mensagem = $dados['CHAT_MENSAGEM'];
+                unset($dados['CHAT_MENSAGEM']);
                 //Verifica se tem algum valor proibido
                 foreach ($dados as $key => $value) {
                     $dados[$key] = $this->Check->checarString($value);
                 }
                 $dados += array(
                     'SUP_DT_CADASTRO'=> date('Y-m-d H:i:s'),
-                    'USU_COD_DESTINATARIO' => 'SUPORTE',
                     'SUP_DT_ATUALIZACAO'=> date('0000-00-00 00:00:00'),          
                     'SUP_STATUS'=> 1
                 );
-
-                if($this->Suporte->cadastrar($dados,0)){
-                    $ok = true;
-                    Sessao::alert('OK','Cadastro efetuado com sucesso!','fs-4 alert alert-success');
+                $id = $this->Suporte->cadastrar($dados,0);
+                if($id){
+                    $db = array(
+                        'SUP_COD'=> $id,
+                        'CHAT_DT_CADASTRO'=> date('Y-m-d H:i:s'),  
+                        'CHAT_MENSAGEM' => $mensagem,     
+                        'CHAT_STATUS'=> 1
+                    );
+                    if($this->Chat->cadastrar($db,0)){
+                        $ok = true;
+                        $respota = array(
+                            'COD'=>'OK',
+                            'MENSAGEM' => 'Cadastro efetuado com sucesso!'
+                        );
+                    }else {
+                        $respota = array(
+                            'COD'=>'ERRO',
+                            'MENSAGEM'=> 'ERRO 2- ERRO AO SALVAR SUA MENSAGEM, FALE COM O SUPORTE POR TELEFONE(s)!'
+                        );
+                    }
                 }else{
-                    Sessao::alert('ERRO',' SET4- Erro ao cadastrar, entre em contato com o suporte!','fs-4 alert alert-danger');
+                    //Sessao::alert('ERRO',' SET4- Erro ao cadastrar, entre em contato com o suporte!','fs-4 alert alert-danger');
+                    $respota = array(
+                        'COD'=>'ERRO',
+                        'MENSAGEM'=> 'ERRO 2- ERRO AO CRIAR MENSAGEM, FALE COM O SUPORTE POR TELEFONE(s)!'
+                    );
                 }
+            }else {
+                $respota = array(
+                    'COD'=>'ERRO',
+                    'MENSAGEM'=> 'ERRO 2- DADOS INVÁLIDO(S)!'
+                );
             }
+        }else {
+            $respota = array(
+                'COD'=>'ERRO',
+                'MENSAGEM'=> 'ERRO 2- ACESSO INVÁLIDO(S)!'
+            );
         }
+        echo json_encode($respota);
     }
 }
