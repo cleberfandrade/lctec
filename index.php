d4a8fe488b90c28c8aac04df75278be529176698
cleberfandrade <cleberf.andrade@hotmail.com>
2024-02-25 14:35:32 -0300

    main

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+#Romanos:11:36 Porque dele, e por ele, e para ele, são todas as coisas; glória, pois, a ele eternamente. Amém.
 use Core\Controller;
 
 require_once __DIR__.'/core/Config.php';
@@ -6,3 +7,5 @@ require_once __DIR__.'/vendor/autoload.php';
 
 $Router = new Controller();
 $Router->carregar();
+
+
