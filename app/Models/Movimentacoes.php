06f1f9e0675dd3bac7592eac284bdebdfea77bd7
Cleber F. Andrade <cleberf.andrade@hotmail.com>
2024-03-02 22:56:07 -0300

    main

--- app/Models/Movimentacoes.php
+++ app/Models/Movimentacoes.php
@@ -71,6 +71,17 @@ class Movimentacoes extends Model
             return false;
         }
     }
+    public function listarTodasMovimentacoesProdutos($ver = 0)
+    {
+        $parametros = "M INNER JOIN tb_empresas E ON E.EMP_COD=M.EMP_COD INNER JOIN tb_estoques ES ON ES.EST_COD=M.EST_COD WHERE M.EMP_COD={$this->codEmpresa} AND M.EST_COD={$this->codEstoque} M.PRO_COD={$this->codProduto} AND M.MOV_TIPO={$this->tipo} AND M.MOV_STATUS={$this->status} ORDER BY M.MOV_COD DESC";
+        $campos = "*";
+        $resultado = $this->Model->exibir($parametros, $campos, $ver = 0, $id = false);
+        if ($resultado) {
+            return $resultado;
+        } else {
+            return false;
+        }
+    }
     public function listarTodasPorTipo($ver = 0)
     {
         if ($this->status) {
