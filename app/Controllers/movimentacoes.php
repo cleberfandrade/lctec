a799ddf2582d6c1014925762972a87bcaf2de395
Cleber F. Andrade <cleberf.andrade@hotmail.com>
2024-03-06 07:07:47 -0300

    main

--- app/Controllers/movimentacoes.php
+++ app/Controllers/movimentacoes.php
@@ -273,22 +273,46 @@ class movimentacoes extends View
         $ok = false;      
         //Recupera os dados enviados
         $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-        dump($dados);
-        //REGISTRAR VENDA - MOTIVO 2 => VENDA
-        /* if($dados['MOV_MOTIVO']  == 2){
-            $db_transacao = array(
-                'VEN_DT_CADASTRO'=> date('Y-m-d H:i:s'),
-                'VEN_DT_ATUALIZACAO'=> date('0000-00-00 00:00:00'),   
-                'TRS_TIPO' => 2,
-                'ITS_COD' => $this->dados['produto']['PRO_COD'],
-                'ITS_QUANTIDADE'=> $dados['MOV_QUANTIDADE'],
-                'VEN_TOKEN' => $this->Check->token(10,'',true),  
-                'VEN_ORDEM' => $this->Check->token(10,'',true),          
-                'VEN_STATUS'=> 1
-            );
-        }else {
-            # code...
-        }*/
+        if (isset($_POST) && isset($dados['CADASTRAR_NOVA_MOVIMENTACAO_ESTOQUE'])) {
+            
+            unset($dados['CADASTRAR_NOVA_MOVIMENTACAO_ESTOQUE']);
+         
+            $this->dados['empresa'] = $this->UsuariosEmpresa->setCodEmpresa($_SESSION['EMP_COD'])->setCodUsuario($_SESSION['USU_COD'])->listar(0);
+           
+            if($this->dados['empresa']['USU_COD'] == $dados['USU_COD'] && $this->dados['empresa']['EMP_COD'] == $dados['EMP_COD']){
+                dump($dados);
+                
+                //Verifica se tem algum valor proibido
+                foreach ($dados as $key => $value) {
+                    $dados[$key] = $this->Check->checarString($value);
+                }
+                $dados += array(
+                    'MOV_DT_CADASTRO'=> date('Y-m-d H:i:s'),
+                    'MOV_DT_ATUALIZACAO'=> date('0000-00-00 00:00:00'),             
+                    'MOV_STATUS'=> 1
+                );
+                //REGISTRAR VENDA - MOTIVO 2 => VENDA
+                /* if($dados['MOV_MOTIVO']  == 2){
+                    $db_transacao = array(
+                        'VEN_DT_CADASTRO'=> date('Y-m-d H:i:s'),
+                        'VEN_DT_ATUALIZACAO'=> date('0000-00-00 00:00:00'),   
+                        'TRS_TIPO' => 2,
+                        'ITS_COD' => $this->dados['produto']['PRO_COD'],
+                        'ITS_QUANTIDADE'=> $dados['MOV_QUANTIDADE'],
+                        'VEN_TOKEN' => $this->Check->token(10,'',true),  
+                        'VEN_ORDEM' => $this->Check->token(10,'',true),          
+                        'VEN_STATUS'=> 1
+                    );
+                }else {
+                    # code...
+                }*/
+            }else{
+                Sessao::alert('ERRO',' MOV12 - Acesso inválido(s)!','alert alert-danger');
+            }
+            
+        }else{
+            Sessao::alert('ERRO',' MOV11- Dados inválido(s)!','alert alert-danger');
+        }
         $this->dados['breadcrumb'] = $this->Check->setLink($this->link)->breadcrumb();
         $this->render('admin/estoques/movimentacoes/movimentacoes', $this->dados);
     }
