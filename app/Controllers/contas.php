464b8bad3011aba2eac2a481447141a94c9b27d3
cleberfandrade <cleberf.andrade@hotmail.com>
2024-03-13 16:11:04 -0300

    main

--- app/Controllers/contas.php
+++ app/Controllers/contas.php
@@ -1,8 +1,11 @@
 <?php 
 namespace App\Controllers;
 
+use App\Models\Caixas;
+use App\Models\Classificacoes;
 use App\Models\Empresas;
 use App\Models\Contas as ModelsContas;
+use App\Models\FormasPagamentos;
 use App\Models\Movimentacoes;
 use App\Models\Usuarios;
 use App\Models\UsuariosEmpresa;
@@ -15,7 +18,7 @@ use Libraries\Url;
 class contas extends View
 {
     private $dados = [];
-    private $link,$Financas,$Check,$Usuarios,$UsuariosEmpresa,$Contas,$Movimentacoes;
+    private $link,$Financas,$Check,$Usuarios,$UsuariosEmpresa,$Contas,$Movimentacoes,$FormasPagamentos,$Caixas,$Classificacoes;
     public function __construct()
     {
         Sessao::naoLogado();
@@ -25,10 +28,16 @@ class contas extends View
         $this->Usuarios = new Usuarios;
         $this->UsuariosEmpresa = new UsuariosEmpresa;
         $this->Movimentacoes = new Movimentacoes;
+        $this->Caixas = new Caixas;
+        $this->Classificacoes = new Classificacoes;
+        $this->FormasPagamentos = new FormasPagamentos;
 
         $this->dados['empresa'] = $this->UsuariosEmpresa->setCodEmpresa($_SESSION['EMP_COD'])->setCodUsuario($_SESSION['USU_COD'])->listar(0);
         $this->dados['usuario'] = $this->Usuarios->setCodUsuario($_SESSION['USU_COD'])->listar(0);
         $this->dados['contas'] = $this->Contas->setCodEmpresa($_SESSION['EMP_COD'])->listarTodas();
+        $this->dados['classificacoes'] = $this->Classificacoes->setCodEmpresa($_SESSION['EMP_COD'])->setTipo(4)->listarTodosPorTipo(0);
+        $this->dados['formas_pagamentos'] = $this->FormasPagamentos->setCodEmpresa($_SESSION['EMP_COD'])->listarTodasAtivas(0);
+        $this->dados['caixas'] = $this->Caixas->setCodEmpresa($_SESSION['EMP_COD'])->listarTodos(0);
 
         $this->link[0] = ['link'=> 'admin','nome' => 'PAINEL ADMINISTRATIVO'];
         $this->link[1] = ['link'=> 'financeiro','nome' => 'MÓDULO DE FINANÇAS >>'];
