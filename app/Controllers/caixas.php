8b65851a16a5ff9ebebb4d91c0864d6d6e11e36b
Cleber F. Andrade <cleberf.andrade@hotmail.com>
2024-03-18 10:46:49 -0300

    main

--- app/Controllers/caixas.php
+++ app/Controllers/caixas.php
@@ -53,4 +53,32 @@ class caixas extends View
         $this->dados['breadcrumb'] = $this->Check->setLink($this->link)->breadcrumb();
         $this->render('admin/financeiro/caixas/cadastrar', $this->dados);
     }
+    public function cadastrar()
+    {
+        $this->dados['title'] .= ' CADASTRAR CAIXA DA EMPRESA/NEGÓCIO';
+        $this->link[3] = ['link'=> 'caixas/listar','nome' => 'CADASTRO DE CAIXAS'];
+        $this->dados['breadcrumb'] = $this->Check->setLink($this->link)->breadcrumb();
+        $ok = false;
+        $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+        if (isset($_POST) && isset($dados['CADASTRAR_NOVO_CAIXA'])) {
+            unset($dados['CADASTRAR_NOVO_CAIXA']);
+            if( $this->dados['empresa']['USU_COD'] == $dados['USU_COD'] && $this->dados['empresa']['EMP_COD'] == $dados['EMP_COD']){
+                //Verifica se tem algum valor proibido
+                foreach ($dados as $key => $value) {
+                    $dados[$key] = $this->Check->checarString($value);
+                }
+            }else{
+                Sessao::alert('ERRO',' CAT2 - Dados inválido(s)!','alert alert-danger');
+            }
+        }else{
+            Sessao::alert('ERRO',' CAT1 - Acesso inválido(s)!','alert alert-danger');
+        }
+        if ($ok) {
+            $this->dados['caixas'] = $this->Caixas->setCodEmpresa($_SESSION['EMP_COD'])->listarTodos(0);
+            $this->render('admin/cadastros/caixas/listar', $this->dados);
+        }else {
+            $this->render('admin/cadastros/caixas/cadastrar', $this->dados);
+        }
+    }
+
 }
