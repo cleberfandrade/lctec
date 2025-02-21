9379d31444a685653224051bd8f86fd9aee11adb
Cleber F. Andrade <cleberf.andrade@hotmail.com>
2025-02-21 10:30:05 -0300

    main

--- app/Controllers/producao.php
+++ app/Controllers/producao.php
@@ -14,7 +14,7 @@ use Libraries\Url;
 class producao extends View
 {
     private $dados = [];
-    private $link,$Financas,$Check,$Usuarios,$UsuariosEmpresa,$Colaboradores,$CargosSalarios,$FolhasPagamento,$Divulgacoes,$Desligamentos,$Horarios,$Promocoes,$Recrutamentos,$Treinamentos,$Beneficios;
+    private $link,$Financas,$Check,$Usuarios,$UsuariosEmpresa,$ModulosEmpresa, $Categorias,$Colaboradores,$CargosSalarios,$FolhasPagamento,$Divulgacoes,$Desligamentos,$Horarios,$Promocoes,$Recrutamentos,$Treinamentos,$Beneficios;
     public function __construct()
     {
         Sessao::naoLogado();
@@ -23,10 +23,22 @@ class producao extends View
         $this->Check = new Check;
         $this->Usuarios = new Usuarios;
         $this->UsuariosEmpresa = new UsuariosEmpresa;
+        $this->ModulosEmpresa = new ModulosEmpresa;
+        $this->Categorias = new Categorias;
 
         $this->dados['empresa'] = $this->UsuariosEmpresa->setCodEmpresa($_SESSION['EMP_COD'])->setCodUsuario($_SESSION['USU_COD'])->listar(0);
         $this->dados['usuario'] = $this->Usuarios->setCodUsuario($_SESSION['USU_COD'])->listar(0);
         
+        $this->dados['modulos_empresa'] = $this->ModulosEmpresa->setCodEmpresa($_SESSION['EMP_COD'])->setCodModulo(3)->checarRegistroModuloEmpresa(0);
+
+        if ($this->dados['modulos_empresa'] == 0) {
+            Sessao::alert('OK',' MÓDULO NÃO DISPONÍVEL!','alert alert-danger');
+            Url::redirecionar('admin/painel');
+        }else {
+            $this->dados['modulo'] = $this->ModulosEmpresa->setCodEmpresa($_SESSION['EMP_COD'])->setCodigo(3)->listarModuloEmpresa(0);
+        }
+
+
         $this->link[0] = ['link'=> 'admin','nome' => 'PAINEL ADMINISTRATIVO'];
         $this->link[1] = ['link'=> 'producao','nome' => 'MÓDULO DE PRODUÇÃO >>'];
     }
