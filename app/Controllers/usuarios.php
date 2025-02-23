f635587cd556163b8f0ed3104c20e6732b9da443
Cleber F. Andrade <cleberf.andrade@hotmail.com>
2025-02-23 17:07:15 -0300

    main

--- app/Controllers/usuarios.php
+++ app/Controllers/usuarios.php
@@ -209,7 +209,7 @@ class usuarios extends View
         $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
         $codUsuario = $dados['USU_COD'];
         $codEmpresa = $dados['EMP_COD'];
-        dump($dados);
+     
         if (isset($_POST) && isset($dados['ALTERAR_USUARIO'])) {
             unset($dados['ALTERAR_USUARIO']);
           
