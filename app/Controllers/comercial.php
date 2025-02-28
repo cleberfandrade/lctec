1fa54dd2f3057a49fcd3ca9342745c470ae726d1
Cleber F. Andrade <cleberf.andrade@hotmail.com>
2025-02-28 20:06:27 -0300

    main

---
+++ app/Controllers/comercial.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controllers;
+
+use App\Models\Classificacoes;
+use App\Models\Clientes as ModelsCliente;
+use App\Models\Empresas;
+use App\Models\Enderecos;
+use App\Models\Usuarios;
+use App\Models\UsuariosEmpresa;
+use App\Models\Setores;
+use Core\View;
+use Libraries\Check;
+use Libraries\Sessao;
+
+class comercial extends View
+{
+    private $dados = [];
+
+}
