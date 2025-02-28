1725414c8318edbc16f035684279784f01d17276
Cleber F. Andrade <cleberf.andrade@hotmail.com>
2025-02-28 20:52:25 -0300

    main

--- app/Controllers/comercial.php
+++ app/Controllers/comercial.php
@@ -15,5 +15,33 @@ use Libraries\Sessao;
 class comercial extends View
 {
     private $dados = [];
-
+    public $link,$Enderecos,$Usuarios,$Empresa,$UsuariosEmpresa,$Check,$Clientes,$Setores,$Classificacoes;
+    public function __construct()
+    {
+        Sessao::naoLogado();
+        $this->dados['title'] = 'MÓDULO | COMERCIAL >>';
+        $this->Clientes= new ModelsCliente;
+        $this->Empresa = new Empresas;
+        $this->Enderecos = new Enderecos;
+        $this->Usuarios = new Usuarios;
+        $this->UsuariosEmpresa = new UsuariosEmpresa;
+        $this->Check = new Check;
+        $this->Setores = new Setores;
+        $this->Classificacoes = new Classificacoes;
+        $this->dados['empresa'] = $this->UsuariosEmpresa->setCodEmpresa($_SESSION['EMP_COD'])->setCodUsuario($_SESSION['USU_COD'])->listar(0);
+        $this->dados['usuario'] = $this->Usuarios->setCodUsuario($_SESSION['USU_COD'])->listar(0);
+        $this->dados['clientes'] = $this->Clientes->setCodEmpresa($_SESSION['EMP_COD'])->listarTodos(0);
+        $this->dados['setores'] = $this->Setores->setCodEmpresa($_SESSION['EMP_COD'])->listarTodos(0);
+        $this->dados['classificacoes'] = $this->Classificacoes->setCodEmpresa($_SESSION['EMP_COD'])->listarTodos(0);
+        
+        $this->link[0] = ['link'=> 'admin','nome' => 'PAINEL ADMINISTRATIVO'];
+        $this->link[1] = ['link'=> 'comercial','nome' => 'MÓDULO COMERCIAL'];
+        $this->link[2] = ['link'=> 'listar','nome' => 'GERENCIAR SUAS VENDAS E PEDIDOS'];
+    }
+    public function index()
+    {
+        $this->dados['title'] .= ' COMERCIAL';
+        $this->dados['breadcrumb'] = $this->Check->setLink($this->link)->breadcrumb();
+        $this->render('admin/comercial/comercial', $this->dados);
+    }
 }
