fb105eed05e8198361bd6df855228de362f0de11
Cleber F. Andrade <cleberf.andrade@hotmail.com>
2025-03-01 14:30:33 -0300

    main

--- app/Controllers/fornecedores.php
+++ app/Controllers/fornecedores.php
@@ -138,7 +138,8 @@ class fornecedores extends View
                 $this->dados['fornecedor'] = $this->Fornecedores->setCodEmpresa($dados[2])->setCodigo($dados[3])->listar(0);
         
                 $id = $this->Enderecos->setCodFornecedor($dados[3])->checarEnderecoFornecedor(0);
-
+                dump($id);
+                exit;
                 unset($id[0]['FOR_COD']);
                 $this->dados['fornecedor'] += $id[0];
                 if ($this->dados['fornecedor'] != 0) {
